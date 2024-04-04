9c42bc4dc80b5ccb51171abf40e44d9230c617f5
Fabian Aunta <fabianleonardo2012@gmail.com>
2024-04-04 00:13:51 -0500

    My first commit

---
+++ src/components/ColorList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const ColorList = () => {
+    const colorList = ["#000000", "#893b3b", "#5ba129"];
+    return(
+        <div className="list-group text-center">
+            {
+                colorList.map((color, index) =>
+                <button key={index} type="button"
+                    className="list-group-item text-white list-group-item-action"
+                    aria-current="true"
+                    title="Copiar"
+                    style={{
+                        background: color,
+                        fontWeight: "bolder",
+                    }}>
+                    {color}
+                </button>
+                )
+            }
+        </div>
+    );
+}
+
+export default ColorList;
